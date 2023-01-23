124ca015477455e134fa16134dd48fedfe4e376c
Victor Patrick <victorbernardpatrick@gmail.com>
2023-01-23 15:48:42 +0100

    Corrected helmet

--- src/index.js
+++ src/index.js
@@ -9,14 +9,13 @@ import { HelmetProvider } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+  <HelmetProvider>
   <React.StrictMode>
-    <HelmetProvider>
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  </HelmetProvider>
   </React.StrictMode>
+  </HelmetProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
